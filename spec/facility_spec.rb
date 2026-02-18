66ce66f418b8bd504544cc9277bab2c7b5dfb1ee
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-18 13:54:45 -0600

    add age verification before facility can administer written test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -100,13 +100,26 @@ RSpec.describe Facility do
       
       @facility_1.add_service('Written Test')
       @facility_1.administer_written_test(@registrant_2)
-
+      
       expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false}) 
       
       @registrant_2.earn_permit
       @facility_1.administer_written_test(@registrant_2)
       
       expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false}) 
+    end
+    
+    it 'checks that a registrant is 16 or older before administering written test' do
+      @facility_1.add_service('Written Test')
+      
+      expect(@registrant_3.age).to eq 15
+      expect(@registrant_3.permit?).to eq false
+      
+      @facility_1.administer_written_test(@registrant_3)
+      @registrant_3.earn_permit
+      @facility_1.administer_written_test(@registrant_3)
+      
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false}) 
     end
   end
 end
