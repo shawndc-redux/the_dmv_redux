7e35ae141620ffc9476638e062e698d3b142e365
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-18 17:10:53 -0600

    add functionality for creating new york dmv facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -7,13 +7,13 @@ class FacilityFactory
 
   def create_facilities(locations)
     locations.map do |location|
-      new_facility = Facility.new({
-        name: location[:dmv_office],
-        address: "#{location[:address_li]} #{location[:address_1]} #{location[:city]}, #{location[:state]} #{location[:zip]}",
-        phone: location[:phone]
-      })
-      build_services(new_facility, location[:services_p])
-      facilities << new_facility
+      if location[:state].include?("CO")
+        build_co_facilities(location)
+      elsif location[:state].include?("NY")
+        build_ny_facilities(location)
+      elsif location[:state].include?("MO")
+        build_mo_facilities(location)
+      end
     end
   end
 
@@ -21,5 +21,37 @@ class FacilityFactory
       new_facility.services = 
     services_p.gsub(',' , "")
       .gsub(';' , "").gsub('vehicle' , "").gsub('VIN' , "").split
+  end
+
+  def build_co_facilities(location)
+    new_facility = Facility.new({
+      name: location[:dmv_office],
+      address: "#{location[:address_li]} #{location[:address_1]} #{location[:city]}, #{location[:state]} #{location[:zip]}",
+      phone: location[:phone]
+    })
+    
+    build_services(new_facility, location[:services_p])
+    facilities << new_facility
+  end
+  
+  def build_ny_facilities(location)
+    new_facility = Facility.new({
+      name: location[:office_name],
+      address: "#{location[:street_address_line_1]} #{location[:street_address_line_2]} #{location[:city]}, #{location[:state]} #{location[:zip]}",
+      phone: location[:public_phone_number]
+    })
+
+    facilities << new_facility 
+  end
+  
+  def build_mo_facilities(location)
+    new_facility = Facility.new({
+      name: location[:dmv_office],
+      address: "#{location[:address_li]} #{location[:address_1]} #{location[:city]}, #{location[:state]} #{location[:zip]}",
+      phone: location[:phone]
+    })
+    build_services(new_facility, location[:services_p])
+    facilities << new_facility
+
   end
 end
